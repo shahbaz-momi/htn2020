d749d7de5870979703927474104eb5e7859adbe8
Shahbaz Momi <ssmomi@uwaterloo.ca>
2021-01-16 17:32:24 -0500

    Add iOS boilerplate

---
+++ ios/git-in-the-3d/Shared/git_in_the_3dApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_in_the_3dApp.swift
+//  Shared
+//
+//  Created by Shahbaz Momi on 2021-01-16.
+//
+
+import SwiftUI
+
+@main
+struct git_in_the_3dApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
